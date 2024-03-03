d1229fb6759141c34d5477342929b3b0692144f1
Park Su Bin <psb2199@naver.com>
2024-03-03 18:15:43 +0900

    Revert "캐릭터2삭제"
    This reverts commit 81f75e530ac5aa65e79c351571334602a139c371.

---
+++ Source/TIMERUN/TIMERUNCharacterVer2.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TIMERUNCharacterVer2.h"
+
+// Sets default values
+ATIMERUNCharacterVer2::ATIMERUNCharacterVer2()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 메쉬 불러오기
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Game/Player/Resource/Male_Rigged"));
+	if (MeshAsset.Succeeded()) {
+		UE_LOG(LogTemp, Warning, TEXT("SkeletMesh Spawn Success"));
+		TIMERUNMesh = MeshAsset.Object;
+		GetMesh()->SetSkeletalMesh(TIMERUNMesh);
+	}
+}
+
+// Called when the game starts or when spawned
+void ATIMERUNCharacterVer2::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	HaveGravityGun = false;
+}
+
+// Called every frame
+void ATIMERUNCharacterVer2::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATIMERUNCharacterVer2::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
--- Source/TIMERUN/TIMERUNGameModeBase.cpp
+++ Source/TIMERUN/TIMERUNGameModeBase.cpp
@@ -3,13 +3,13 @@
 
 #include "TIMERUNGameModeBase.h"
 #include "TIMERUNCharacter.h"
-
+#include "TIMERUNCharacterVer2.h"
 #include "TIMERUNPlayerController.h"
 
 ATIMERUNGameModeBase::ATIMERUNGameModeBase()
 {	
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/BP_TIMERUNCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/BP_TIMERUNCharacterVer2"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
