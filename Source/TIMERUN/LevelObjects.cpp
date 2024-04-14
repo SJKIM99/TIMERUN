96ec54f6ed0f5673133ccc2a644cb80bcd01047b
Park Su Bin <psb2199@naver.com>
2024-04-14 10:38:06 +0900

    materail

---
+++ Source/TIMERUN/LevelObjects.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelObjects.h"
+
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ALevelObjects::ALevelObjects()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	RootComponent = StaticMeshComponent;
+
+	TimeLevel = 0;
+}
+
+// Called when the game starts or when spawned
+void ALevelObjects::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MyCharacter = Cast<ATIMERUNCharacter>(UGameplayStatics::GetPlayerCharacter(this, 0));
+	
+}
+
+// Called every frame
+void ALevelObjects::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	TimeLevel = MyCharacter->my_time;
+
+}
+
