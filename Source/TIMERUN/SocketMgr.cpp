3570ec3c44d95a43bbeca4e72d60fcde272fd08e
SJKIM99 <sungjun4264@naver.com>
2024-05-08 02:02:13 +0900

    .

--- Source/TIMERUN/SocketMgr.cpp
+++ Source/TIMERUN/SocketMgr.cpp
@@ -23,7 +23,7 @@ bool SocketMgr::ConnectLoginServer()
 	SOCKADDR_IN server_addr;
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(8000);
-	server_addr.sin_addr.s_addr = inet_addr("172.30.1.78");
+	server_addr.sin_addr.s_addr = inet_addr("210.99.254.132");
 	//server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	nRet = connect(login_socket, (sockaddr*)&server_addr, sizeof(sockaddr));
@@ -46,7 +46,7 @@ bool SocketMgr::ConnectIngameServer()
 	SOCKADDR_IN server_addr;
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(9000);
-	server_addr.sin_addr.s_addr = inet_addr("172.30.1.78");
+	server_addr.sin_addr.s_addr = inet_addr("210.99.254.132");
 	//server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	nRet = connect(ingame_socket, (sockaddr*)&server_addr, sizeof(sockaddr));
