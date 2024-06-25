1584ba4efb2376ae9cdcc5d44cd2b5b62c5d2b9d
SJKIM99 <sungjun4264@naver.com>
2024-06-25 17:37:45 +0900

    modify IP address

--- Source/TIMERUN/SocketMgr.cpp
+++ Source/TIMERUN/SocketMgr.cpp
@@ -23,7 +23,8 @@ bool SocketMgr::ConnectLoginServer()
 	SOCKADDR_IN server_addr;
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(8000);
-	server_addr.sin_addr.s_addr = inet_addr("210.99.254.132");
+	server_addr.sin_addr.s_addr = inet_addr("25.52.149.86");	//하마치
+	//server_addr.sin_addr.s_addr = inet_addr("210.99.254.132");	//포트포워딩
 	//server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	nRet = connect(login_socket, (sockaddr*)&server_addr, sizeof(sockaddr));
@@ -46,7 +47,8 @@ bool SocketMgr::ConnectIngameServer()
 	SOCKADDR_IN server_addr;
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(9000);
-	server_addr.sin_addr.s_addr = inet_addr("210.99.254.132");
+	server_addr.sin_addr.s_addr = inet_addr("25.52.149.86");	//하마치
+	//server_addr.sin_addr.s_addr = inet_addr("210.99.254.132");	//포트포워딩
 	//server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	nRet = connect(ingame_socket, (sockaddr*)&server_addr, sizeof(sockaddr));
